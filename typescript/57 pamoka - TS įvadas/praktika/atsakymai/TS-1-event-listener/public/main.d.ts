87eda5061060ad5b19dbd24b15a550571e228fa3
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-03-09 10:12:53 +0200

    feat(57): initial files

---
+++ typescript/57 pamoka - TS įvadas/praktika/atsakymai/TS-1-event-listener/public/main.d.ts
@@ -0,0 +1,4 @@
+declare const inputField: HTMLInputElement | null;
+declare const result: HTMLParagraphElement | null;
+declare const handleFieldChange: EventListener;
+//# sourceMappingURL=main.d.ts.map
---
+++ typescript/57 pamoka - TS įvadas/praktika/atsakymai/TS-1-primitive-types/public/main.d.ts
@@ -0,0 +1,8 @@
+declare const height: number;
+declare const weight: number;
+declare const fullname: string;
+declare const surname: string;
+declare const age: number;
+declare const wife: null;
+declare const innerAnimal: undefined;
+//# sourceMappingURL=main.d.ts.map
