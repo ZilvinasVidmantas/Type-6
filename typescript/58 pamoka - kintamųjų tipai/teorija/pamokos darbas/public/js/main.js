df18b80a9255f76c2e815591d0d63cb9acadd016
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-03-10 11:52:17 +0200

    feat(58): displayed students as HTML table

--- typescript/58 pamoka - kintamųjų tipai/teorija/pamokos darbas/public/js/main.js
+++ typescript/58 pamoka - kintamųjų tipai/teorija/pamokos darbas/public/js/main.js
@@ -1,22 +1,84 @@
 "use strict";
-const students = [
+const allStudents = [
     {
+        id: '1',
         name: 'Serbentautas',
         surname: 'Bordiūras',
         course: 2,
         hasTakenGapYear: false,
+        semesters: [
+            {
+                id: '1',
+                title: 1,
+                modules: [
+                    {
+                        id: '1',
+                        title: 'Šonaslydis',
+                        marks: [9, 10, 9, 8],
+                    },
+                    {
+                        id: '2',
+                        title: 'Pandagų inspekcijos inžinerija',
+                        marks: [7, 8, 8, 8, 7],
+                    },
+                ],
+            },
+            {
+                id: '2',
+                title: 2,
+                modules: [
+                    {
+                        id: '3',
+                        title: 'Matorų čipavimas',
+                        marks: [6, 6, 5, 5],
+                    },
+                ],
+            },
+            {
+                id: '3',
+                title: 3,
+                modules: [
+                    {
+                        id: '4',
+                        title: 'Langų valymas',
+                        marks: [10, 10, 10, 10],
+                    },
+                    {
+                        id: '5',
+                        title: 'Juodoji buhalterija',
+                        marks: [9, 8, 10, 7, 7],
+                    },
+                ],
+            },
+        ],
     },
     {
-        name: 'Valorija',
-        surname: 'Gutpėda',
-        course: 3,
-        hasTakenGapYear: true,
-    },
-    {
+        id: '9',
         name: 'Fanalas',
         surname: 'Bamblys',
         course: 1,
         hasTakenGapYear: false,
     },
 ];
+const tableBodySelector = '#students-table>tbody';
+const tableBody = document.querySelector(tableBodySelector);
+if (!tableBody) {
+    throw new Error(`Table with selector '${tableBodySelector}' was not found`);
+}
+const createStudentRowString = ({ id, name, surname, course, hasTakenGapYear, }) => `
+  <tr>
+    <th scope="row">${id}</th>
+    <td>${name}</td>
+    <td>${surname}</td>
+    <td>${course}</td>
+    <td>${hasTakenGapYear ? 'Yes' : 'No'}</td>
+  </tr>`;
+const displayStudents = (students) => {
+    tableBody.innerHTML = '';
+    students.forEach((x) => {
+        const rowString = createStudentRowString(x);
+        tableBody.innerHTML += rowString;
+    });
+};
+displayStudents(allStudents);
 //# sourceMappingURL=main.js.map
--- typescript/58 pamoka - kintamųjų tipai/teorija/pamokos darbas/src/main.ts
+++ typescript/58 pamoka - kintamųjų tipai/teorija/pamokos darbas/src/main.ts
@@ -1,10 +1,3 @@
-type Student = {
-  name: string,
-  surname: string,
-  course: number,
-  hasTakenGapYear: boolean,
-};
-
 type Module = {
   id: string,
   title: string,
@@ -13,27 +6,109 @@ type Module = {
 
 type Semester = {
   id: string,
-  title: string,
+  title: number,
   modules: Module[],
 };
 
-const students: Student[] = [
+type Student = {
+  id: string,
+  name: string,
+  surname: string,
+  course: number,
+  hasTakenGapYear: boolean,
+  semesters?: Semester[],
+};
+
+const allStudents: Student[] = [
   {
+    id: '1',
     name: 'Serbentautas',
     surname: 'Bordiūras',
     course: 2,
     hasTakenGapYear: false,
+    semesters: [
+      {
+        id: '1',
+        title: 1,
+        modules: [
+          {
+            id: '1',
+            title: 'Šonaslydis',
+            marks: [9, 10, 9, 8],
+          },
+          {
+            id: '2',
+            title: 'Pandagų inspekcijos inžinerija',
+            marks: [7, 8, 8, 8, 7],
+          },
+        ],
+      },
+      {
+        id: '2',
+        title: 2,
+        modules: [
+          {
+            id: '3',
+            title: 'Matorų čipavimas',
+            marks: [6, 6, 5, 5],
+          },
+        ],
+      },
+      {
+        id: '3',
+        title: 3,
+        modules: [
+          {
+            id: '4',
+            title: 'Langų valymas',
+            marks: [10, 10, 10, 10],
+          },
+          {
+            id: '5',
+            title: 'Juodoji buhalterija',
+            marks: [9, 8, 10, 7, 7],
+          },
+        ],
+      },
+    ],
   },
   {
-    name: 'Valorija',
-    surname: 'Gutpėda',
-    course: 3,
-    hasTakenGapYear: true,
-  },
-  {
+    id: '9',
     name: 'Fanalas',
     surname: 'Bamblys',
     course: 1,
     hasTakenGapYear: false,
   },
 ];
+
+const tableBodySelector = '#students-table>tbody';
+const tableBody = document.querySelector<HTMLTableElement>(tableBodySelector);
+
+if (!tableBody) {
+  throw new Error(`Table with selector '${tableBodySelector}' was not found`);
+}
+
+const createStudentRowString = ({
+  id,
+  name,
+  surname,
+  course,
+  hasTakenGapYear,
+}: Student): string => `
+  <tr>
+    <th scope="row">${id}</th>
+    <td>${name}</td>
+    <td>${surname}</td>
+    <td>${course}</td>
+    <td>${hasTakenGapYear ? 'Yes' : 'No'}</td>
+  </tr>`;
+
+const displayStudents = (students: Student[]): void => {
+  tableBody.innerHTML = '';
+  students.forEach((x) => {
+    const rowString = createStudentRowString(x);
+    tableBody.innerHTML += rowString;
+  });
+};
+
+displayStudents(allStudents);
