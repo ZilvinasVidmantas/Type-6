2433e50923d8f3815fe829d5e36c35abba8865f1
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-03-10 09:11:24 +0200

    task(58): create Object types

--- typescript/58 pamoka - kintamųjų tipai/teorija/pamokos darbas/public/js/main.js
+++ typescript/58 pamoka - kintamųjų tipai/teorija/pamokos darbas/public/js/main.js
@@ -1,3 +1,22 @@
 "use strict";
-console.log('veikia');
+const students = [
+    {
+        name: 'Serbentautas',
+        surname: 'Bordiūras',
+        course: 2,
+        hasTakenGapYear: false
+    },
+    {
+        name: 'Valorija',
+        surname: 'Gutpėda',
+        course: 3,
+        hasTakenGapYear: true
+    },
+    {
+        name: 'Fanalas',
+        surname: 'Bamblys',
+        course: 1,
+        hasTakenGapYear: false
+    }
+];
 //# sourceMappingURL=main.js.map
--- typescript/58 pamoka - kintamųjų tipai/teorija/pamokos darbas/src/main.ts
+++ typescript/58 pamoka - kintamųjų tipai/teorija/pamokos darbas/src/main.ts
@@ -1,1 +1,42 @@
-console.log('veikia')
+type Student = {
+  name: string,
+  surname: string,
+  course: number,
+  hasTakenGapYear: boolean,
+};
+
+const students: Student[] = [
+  {
+    name: 'Serbentautas',
+    surname: 'Bordiūras',
+    course: 2,
+    hasTakenGapYear: false
+  },
+  {
+    name: 'Valorija',
+    surname: 'Gutpėda',
+    course: 3,
+    hasTakenGapYear: true
+  },
+  {
+    name: 'Fanalas',
+    surname: 'Bamblys',
+    course: 1,
+    hasTakenGapYear: false
+  }
+];
+
+
+/*
+  Sukurkite Tipus:
+    * Module
+      * id: string
+      * title: string
+      * marks: number[]
+
+    * Semester
+      * id: string
+      * title: string
+      * modules: Module[]
+*/
+
