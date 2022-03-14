a03a27b86a6064c6348120ca089f43e39fc9cd94
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-03-14 10:22:03 +0200

    task(64): table title prop task

--- typescript/64 pamoka - Tipų aprašymo užtvirtinimas/pamokos darbas/src/main.ts
+++ typescript/64 pamoka - Tipų aprašymo užtvirtinimas/pamokos darbas/src/main.ts
@@ -9,3 +9,12 @@ if (!rootElement) {
 
 const productsTable = new ProductsTable(products);
 rootElement.appendChild(productsTable.render());
+
+/*
+  įgalinkite logiką, jog būtų galima nurodyti lentelės pavadinimą:
+    * ProductsTable komponente, priimkite naują parametrą title
+      * Šį parametrą priskirkite ProductsTable objektui(instance'ui)
+    * ProductsTable.header statinę savybę perrašykite, jog ji būtų objekto('instance') metodas,
+      ir panaudokite konstruktoriuje priskirtą 'title' reikšmę
+  10:30
+*/
