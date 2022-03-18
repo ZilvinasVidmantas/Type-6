52f3f098d326564757cfb16ea093e3448db097c3
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-03-18 11:18:43 +0200

    feat(68): added problemic tasks

---
+++ typescript/68 pamoka - kartojimas 2/pamokos darbas/src/main.ts
@@ -0,0 +1,59 @@
+console.groupCollapsed('6. Parašykite funkciją, kuri atspausdina paskutinio masyvo elemento indeksą');
+{
+  // ...sprendimas ir spausdinimas
+}
+
+console.groupCollapsed('13. Parašykite funkciją, kuri atspausdina kiekvieno masyvo elemento indekso ir reikšmių poras vienoje eilutėje, tokiu formatu:');
+//  [0]=>17 [1]=>8 [2]=>88 ..
+{
+  // ...sprendimas ir spausdinimas
+}
+console.groupEnd();
+
+console.groupCollapsed('1 - https://edabit.com/challenge/nuXdWHAoHv9y38sn7');
+{
+
+}
+console.groupEnd();
+
+console.groupCollapsed('2 - https://edabit.com/challenge/9KEKJG5PZTFmG3Zau');
+{
+
+}
+console.groupEnd();
+
+console.groupCollapsed('3 - https://edabit.com/challenge/48EJWLhF224na8po3');
+{
+
+}
+console.groupEnd();
+
+console.groupCollapsed('4 - https://edabit.com/challenge/i6YqzHcSiPiEQKjeX');
+{
+
+}
+console.groupEnd();
+
+console.groupCollapsed('5 - https://edabit.com/challenge/8s2jy9hR2TAeQinKD');
+{
+
+}
+console.groupEnd();
+
+console.groupCollapsed('6 - https://edabit.com/challenge/pPNAs5PvB3WvnDwDM');
+{
+
+}
+console.groupEnd();
+
+console.groupCollapsed('7 - https://edabit.com/challenge/QXWM2oo7rQNiyDsip');
+{
+
+}
+console.groupEnd();
+
+console.groupCollapsed('8 - https://edabit.com/challenge/pLNavsePxJ87t9Nak');
+{
+
+}
+console.groupEnd();
