e1874a8d0b656818902ee8921288eed2141a6f7f
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-03-21 09:07:08 +0200

    feat(69): Incapsulation explained

--- typescript/69 pamoka - kartojimas 3 ir bendriniai tipai/pamokos darbas/src/main.ts
+++ typescript/69 pamoka - kartojimas 3 ir bendriniai tipai/pamokos darbas/src/main.ts
@@ -47,7 +47,7 @@ console.groupCollapsed('1. Kas yra ir kaip veikia Partial<Type>?');
 }
 console.groupEnd();
 
-console.groupCollapsed('2. Anoniminės funkcijos');
+console.groupCollapsed('2. Kas yra  ir kaip naudojamos noniminės funkcijos?');
 {
   // Anonimė funkcija, tai naujo funkcijos deklaravimas kviečiant kitą funkciją;
 
@@ -65,5 +65,65 @@ console.groupCollapsed('2. Anoniminės funkcijos');
     numberSum,
     numberSum2,
   });
+}
+console.groupEnd();
+
+console.groupCollapsed('3. OOP inkapsuliacija. Kaip ir kada naudoti?');
+{
+  /*
+    Inkapsuliacija tai reikšmės/funkcijos pasiekiamumo ribojimas.
+
+    Leidami programuotojams pasiekti reikšmę tiesiogiai (be ribojimų) rizikuojame,
+    jog tai bus padaroma nekorektiškai.
+
+    Inkapsuliacija padeda mums užtrikti ir suvaldyti reikšmių naudojimą/priskyrimą
+  */
+
+  class Person {
+    public name: string;
+
+    public surname: string;
+
+    private age: number;
+
+    public constructor(name: string, surname: string, age: number) {
+      this.name = name;
+      this.surname = surname;
+      this.age = age;
+    }
+
+    // Funkcija, kuri inkapsuliuoja reikšmės gavimą
+    public getAge = (): number => this.age;
+
+    // Funkcija, kuri inkapsuliuoja reikšmės priskyrimą
+    public setAge = (newAge: number): void => {
+      if (newAge > 1500) {
+        console.error('Eik peklon vampyre!');
+        return;
+      }
+      if (newAge < 1) {
+        console.error('Tu dar negimei');
+        return;
+      }
+
+      this.age = newAge;
+    };
+  }
+
+  const person1 = new Person('Kibiras', 'Dundauskas', 12);
+  const person2 = new Person('Lempa', 'Daužaitė', 15);
+  const people: Person[] = [
+    person1,
+    person2,
+  ];
+
+  // Nustatoma amžiaus savybė naudojant set'erius
+
+  person1.setAge(16000);
+  person1.setAge(16);
+  person2.setAge(-55);
+  // person2.setAge(1);
+
+  console.table(people);
 }
 console.groupEnd();
