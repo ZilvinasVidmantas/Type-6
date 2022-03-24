1c068ab240a08eba937a85160c4919a2356e1a5f
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-03-24 12:44:19 +0200

    feat(72): asnwers and task correction

--- typescript/72 pamoka - kartojimas ir paveldimumas/individualiai/atsakymai/src/main.ts
+++ typescript/72 pamoka - kartojimas ir paveldimumas/individualiai/atsakymai/src/main.ts
@@ -1,3 +1,6 @@
+import LandVehicle from './land-vehicle.js';
+import AirVehicle from './air-vehicle.js';
+import WaterVehicle from './water-vehicle.js';
 /*
   1. Sukurkite klasę WaterVehicle, kuri turėtų savybes:
     brand: string,
@@ -23,7 +26,7 @@
     
   5. Vehicle klasėje aprašykite metodą 'public toStringVehicle(): string' kuris suformuotų pagindinė informaciją(brand, model, year)
 
-  6. Vehicle klasėje pakeiskite metodą 'public toStringVehicle(): string' į 'PROTECTED toString(): toStringVehicle'. Kiekvienoje paveldinčioje
+  6. Vehicle klasėje pakeiskite metodą 'public toStringVehicle(): string' į 'PROTECTED toStringVehicle(): string'. Kiekvienoje paveldinčioje
     klasėje sukurkite metodus 'public toString(): string', kurios naudotų tėvinės klasės metodus metodą 'PROTECTED toStringVehicle(): string'
     tam kad suformuoti pilną savosios klasės reprezentaciją
 
@@ -31,3 +34,50 @@
 
   8. Išskaidykite kodą atskirais failais
 */
+
+const vehicles: (LandVehicle | AirVehicle | WaterVehicle)[] = [
+  new LandVehicle({
+    tires: ['Delux 200 m&s', 'Delux 200 m&s', 'Fairtex 200 m&s', 'Fairtex 200 m&s']
+  }, {
+    brand: 'Toyota',
+    model: 'Corola',
+    year: 2005,
+  }),
+  new LandVehicle({
+    tires: ['Delux 200 m&s', 'Delux 200 m&s', 'Fairtex 200 m&s', 'Fairtex 200 m&s']
+  }, {
+    brand: 'Nisan',
+    model: 'Qashqai',
+    year: 2007,
+  }),
+  new AirVehicle({
+    maxAltitude: 7000,
+  }, {
+    brand: 'Jeti',
+    model: 'Nitro',
+    year: 2015,
+  }),
+  new AirVehicle({
+    maxAltitude: 5000,
+  }, {
+    brand: 'Falcon',
+    model: 'Bamasi',
+    year: 2012,
+  }),
+  new WaterVehicle({
+    maxDepth: 50,
+  }, {
+    brand: 'Sailor',
+    model: 'Ocean 3000',
+    year: 2011,
+  }),
+  new WaterVehicle({
+    maxDepth: 70,
+  }, {
+    brand: 'LandScraper',
+    model: 'Fagotti',
+    year: 202,
+  }),
+];
+
+vehicles.forEach(v => console.log(v.toString()));
