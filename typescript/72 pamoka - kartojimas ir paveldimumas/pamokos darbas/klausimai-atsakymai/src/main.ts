4e09e85d4e8bc3e86df0df0b5bd114d10a1287c9
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-03-24 08:04:16 +0200

    init(72): revision and inheritance

---
+++ typescript/72 pamoka - kartojimas ir paveldimumas/pamokos darbas/klausimai-atsakymai/src/main.ts
@@ -0,0 +1,11 @@
+/* eslint-disable no-empty */
+/* eslint-disable prefer-promise-reject-errors */
+/* eslint-disable max-classes-per-file */
+/* eslint-disable no-console */
+/* eslint-disable no-lone-blocks */
+
+console.group('1.');
+{
+
+}
+console.groupEnd();
---
+++ typescript/72 pamoka - kartojimas ir paveldimumas/pamokos darbas/paveldimumas/src/processor.ts
@@ -0,0 +1,7 @@
+class Processor {
+  constructor(
+    private title: string,
+  ) { }
+}
+
+export default Processor;
