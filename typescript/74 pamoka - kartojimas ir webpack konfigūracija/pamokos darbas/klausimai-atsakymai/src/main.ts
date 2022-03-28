a8de9b225ee76df6a022634122d46d9419945055
Zilwa <zilvinas.vidmantas.93@gmail.com>
2022-03-28 08:05:18 +0300

    init(74): revision and webpack project configuration

---
+++ typescript/74 pamoka - kartojimas ir webpack konfigūracija/pamokos darbas/klausimai-atsakymai/src/main.ts
@@ -0,0 +1,12 @@
+/* eslint-disable no-inner-declarations */
+/* eslint-disable no-empty */
+/* eslint-disable prefer-promise-reject-errors */
+/* eslint-disable max-classes-per-file */
+/* eslint-disable no-console */
+/* eslint-disable no-lone-blocks */
+
+console.groupCollapsed('1. Destrukturizacija');
+{
+
+}
+console.groupEnd();
