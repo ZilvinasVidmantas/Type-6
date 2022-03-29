5028933a433c1747ec4a33de59f3e71d1f665a73
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-03-29 12:01:30 +0300

    feat(75): data rendering

--- typescript/75 pamoka - duomenų atvaizdavimas/pamokos darbas/src/main.ts
+++ typescript/75 pamoka - duomenų atvaizdavimas/pamokos darbas/src/main.ts
@@ -2,7 +2,18 @@ import ProductsCollection from './helpers/products-collection';
 import products from './data/products';
 import categories from './data/categories';
 import productsCategories from './data/products-categories';
+import stringifyProperties from './helpers/stringify-properties';
+import Table from './components/table';
 
 const productsCollection = new ProductsCollection(products, categories, productsCategories);
 
-console.table(productsCollection.all);
+const stringifiedData = productsCollection.all.map(stringifyProperties);
+
+const productsTable = new Table(stringifiedData);
+
+const root = document.querySelector('#root') as HTMLElement;
+const container = document.createElement('div');
+container.className = 'container my-5';
+
+container.append(productsTable.htmlElement);
+root.append(container);
