3af9883f3e492ddaf66f739f417d99f223b3a9e9
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-04-01 08:38:27 +0300

    feat(78): OOP incapsulation answer

--- typescript/78 pamoka - duomenų atnaujinimas ir atnaujino UX/pamokos darbas/klausimai-atsakymai/src/main.ts
+++ typescript/78 pamoka - duomenų atnaujinimas ir atnaujino UX/pamokos darbas/klausimai-atsakymai/src/main.ts
@@ -4,9 +4,41 @@
 /* eslint-disable max-classes-per-file */
 /* eslint-disable no-console */
 /* eslint-disable no-lone-blocks */
-console.group('1.');
+console.group('1. OOP inkapsuliacija');
 {
-  
+  class Person {
+    constructor(
+      public name: string,
+      public surname: string,
+      private p_age: number,
+    ) { }
+
+    // public setAge = (newAge: number): void => {
+    //   if (newAge < 0 || newAge > 150) {
+    //     console.error('netinkamas amžius');
+    //     return;
+    //   }
+    //   this.p_age = newAge;
+    // };
+    public set age(newAge: number) {
+      if (newAge < 0 || newAge > 150) {
+        console.error('netinkamas amžius');
+        return;
+      }
+      this.p_age = newAge;
+    }
+
+    // public getAge = (): number => this.p_age;
+    public get age() {
+      return this.p_age;
+    }
+  }
+
+  const person = new Person('Kuprius', 'Pilkaitis', 19);
+
+  person.age = 20;
+
+  console.log(person.age);
 }
 
 console.groupEnd();
