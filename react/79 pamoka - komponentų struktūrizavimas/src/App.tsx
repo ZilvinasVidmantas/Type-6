54fce0a4d3ea264a59dd3a9a34db497a4654ccf7
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-04-04 10:19:32 +0300

    feat(79): initial react TS project setup

--- react/79 pamoka - komponentų struktūrizavimas/src/App.tsx
+++ react/79 pamoka - komponentų struktūrizavimas/src/App.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 
-const App: React.FC = () => {
-  return (
-    <div>
-      Cia yra div elementas
-    </div>
-  );
-}
+const App: React.FC = () => (
+  <div>
+    Cia yra div elementas
+  </div>
+);
 
 export default App;
