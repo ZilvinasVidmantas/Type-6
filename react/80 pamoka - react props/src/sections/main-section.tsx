0518f3f59748fa67174507132e997a46719587f6
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-04-05 10:45:04 +0300

    feat(80): main section view

--- react/80 pamoka - react props/src/sections/main-section.tsx
+++ react/80 pamoka - react props/src/sections/main-section.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import theme from '../theme';
 
 const style: React.CSSProperties = {
-  paddingTop: theme.navbarHeight,
+  height: '100vh',
+  backgroundImage: 'url(/assets/main-section-cover.jpg)',
+  backgroundSize: 'cover',
+  backgroundPosition: 'center',
 };
-
-const title = 'Cia yra programos pavadinimas';
-const paragraphText = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, asperiores? Laboriosam unde accusantium quaerat numquam quos atque a, dicta dolores quod recusandae ullam illum sint inventore maxime blanditiis exercitationem impedit veniam, non nam autem molestias laborum quisquam! Saepe placeat eos libero accusamus. Cumque hic nihil vitae sequi nesciunt magnam accusantium maxime doloribus autem perspiciatis labore atque ex corrupti, qui molestias laboriosam sapiente laudantium impedit deserunt, nam consequuntur culpa. Fuga rem quisquam, voluptas ad eum quidem necessitatibus a ex quo illum. Quo eligendi quidem voluptates temporibus pariatur aperiam iste unde fugiat magnam nulla reiciendis tempora, cupiditate, officiis modi consequuntur. Iusto, assumenda.';
 
 const MainSection: React.FC = () => (
   <main style={style} />
