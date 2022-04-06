554f718eea6c690a260790bd0e62afa099fd3a3a
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-04-06 10:20:14 +0300

    refactor(81): navbar styled

--- react/81 pamoka - styled components/src/index.tsx
+++ react/81 pamoka - styled components/src/index.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { ThemeProvider } from 'styled-components';
 
+import GlobalCSS from './theme/global-css';
 import theme from './theme';
 import App from './app';
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalCSS />
     <ThemeProvider theme={theme}>
       <App />
     </ThemeProvider>
