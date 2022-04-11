120443fe91effda5fae2ff05f979a749c9240905
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-04-11 10:27:10 +0300

    feat(84): initial router

--- react/84 pamoka - mui/src/app.tsx
+++ react/84 pamoka - mui/src/app.tsx
@@ -1,5 +1,20 @@
 import React from 'react';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from 'react-router-dom';
 
-const App: React.FC = () => <div>App</div>;
+import HomePage from './pages/home-page';
+import ButtonsPage from './pages/buttons-page';
+
+const App: React.FC = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/buttons" element={<ButtonsPage />} />
+    </Routes>
+  </BrowserRouter>
+);
 
 export default App;
