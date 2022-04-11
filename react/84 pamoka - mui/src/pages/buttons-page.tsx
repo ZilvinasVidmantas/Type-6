188b397e9b4294063636a75d9bc9b92c545e7e48
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-04-11 11:28:44 +0300

    feat(84): simple buttons

--- react/84 pamoka - mui/src/pages/buttons-page.tsx
+++ react/84 pamoka - mui/src/pages/buttons-page.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import { Typography, Button, Box } from '@mui/material';
 
 const ButtonsPage: React.FC = () => (
-  <div>ButtonsPage</div>
+  <Box
+    component="main"
+    sx={{
+      bgcolor: 'grey.500',
+      ':hover': {
+        bgcolor: 'primary.main',
+      },
+    }}
+  >
+    <Typography component="h1" variant="h1">Buttons</Typography>
+    <Button variant="contained">contained</Button>
+    <Button variant="text">text</Button>
+    <Button variant="outlined">outlined</Button>
+  </Box>
 );
 
 export default ButtonsPage;
