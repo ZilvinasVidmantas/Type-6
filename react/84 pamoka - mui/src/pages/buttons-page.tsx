b0e1236610348523ac012546249ddaaf3712fb70
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-04-11 11:56:56 +0300

    feat(84): button examples

--- react/84 pamoka - mui/src/pages/buttons-page.tsx
+++ react/84 pamoka - mui/src/pages/buttons-page.tsx
@@ -2,19 +2,15 @@ import React from 'react';
 import { Typography, Button, Box } from '@mui/material';
 
 const ButtonsPage: React.FC = () => (
-  <Box
-    component="main"
-    sx={{
-      bgcolor: 'grey.500',
-      ':hover': {
-        bgcolor: 'primary.main',
-      },
-    }}
-  >
+  <Box component="main">
     <Typography component="h1" variant="h1">Buttons</Typography>
-    <Button variant="contained">contained</Button>
-    <Button variant="text">text</Button>
-    <Button variant="outlined">outlined</Button>
+    <Typography component="a" href="https://mui.com/material-ui/react-button/" target="blank">Documentation</Typography>
+    <Box sx={{ mt: 4 }}>
+      <Button variant="contained">contained</Button>
+      <Button variant="text">text</Button>
+      <Button variant="outlined">outlined</Button>
+      <Button variant="outlined" href="https://google.lt">Google</Button>
+    </Box>
   </Box>
 );
 
