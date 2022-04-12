c93d3e7d208c55ebba9ee825990a0284e6e43777
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-04-12 08:58:14 +0300

    feat(84): ThemeProvider

--- react/85 pamoka - mui styling/src/index.tsx
+++ react/85 pamoka - mui styling/src/index.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ThemeProvider } from '@mui/material';
+
+import theme from './styles/theme';
 import App from './app';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
---
+++ react/85 pamoka - mui styling/src/styles/theme.ts
@@ -0,0 +1,14 @@
+import { createTheme } from '@mui/material';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#fc5203',
+      light: '#ff6f2b',
+      dark: '#e04800',
+      contrastText: '#ffffff',
+    },
+  },
+});
+
+export default theme;
