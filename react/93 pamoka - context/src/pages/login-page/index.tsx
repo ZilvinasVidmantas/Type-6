25ed2dd63c06921ad8ef7dc94d22213ce2a48c2c
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-04-25 11:20:59 +0300

    feat(93): login form view

--- react/93 pamoka - context/src/pages/login-page/index.tsx
+++ react/93 pamoka - context/src/pages/login-page/index.tsx
@@ -1,9 +1,53 @@
-import { Container, Typography } from '@mui/material';
 import React from 'react';
+import {
+  Box,
+  Button,
+  Container, Paper, TextField, Typography,
+} from '@mui/material';
+import SecurityIcon from '@mui/icons-material/Security';
 
 const LoginPage: React.FC = () => (
-  <Container>
-    <Typography>Login</Typography>
+  <Container sx={{ pt: 20 }}>
+    <Paper
+      elevation={3}
+      sx={{
+        display: 'flex',
+        mx: 'auto',
+        flexDirection: 'column',
+        alignItems: 'center',
+        gap: 1,
+        p: 3,
+        width: 400,
+      }}
+    >
+      <SecurityIcon color="primary" sx={{ fontSize: 45 }} />
+      <Typography component="h1" variant="h4">
+        Login
+      </Typography>
+
+      <Box sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        gap: 2,
+        width: 1 / 1,
+        my: 2,
+      }}
+      >
+        <TextField
+          type="email"
+          label="Email"
+          fullWidth
+        />
+        <TextField
+          type="password"
+          label="Password"
+          fullWidth
+        />
+      </Box>
+
+      <Button variant="contained" size="large">Login</Button>
+
+    </Paper>
   </Container>
 );
 
