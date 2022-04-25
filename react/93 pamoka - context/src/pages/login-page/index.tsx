c32bd632c309fbfd08aa413937aa2e104e78069e
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-04-25 11:41:27 +0300

    feat(93): handle form submit

--- react/93 pamoka - context/src/pages/login-page/index.tsx
+++ react/93 pamoka - context/src/pages/login-page/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Box,
   Button,
@@ -6,49 +6,67 @@ import {
 } from '@mui/material';
 import SecurityIcon from '@mui/icons-material/Security';
 
-const LoginPage: React.FC = () => (
-  <Container sx={{ pt: 20 }}>
-    <Paper
-      elevation={3}
-      sx={{
-        display: 'flex',
-        mx: 'auto',
-        flexDirection: 'column',
-        alignItems: 'center',
-        gap: 1,
-        p: 3,
-        width: 400,
-      }}
-    >
-      <SecurityIcon color="primary" sx={{ fontSize: 45 }} />
-      <Typography component="h1" variant="h4">
-        Login
-      </Typography>
+const LoginPage: React.FC = () => {
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
 
-      <Box sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        gap: 2,
-        width: 1 / 1,
-        my: 2,
-      }}
+  const handleSubmit: React.FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+
+    console.log({
+      email,
+      password,
+    });
+  };
+
+  return (
+    <Container sx={{ pt: 3 }}>
+      <Paper
+        component="form"
+        elevation={3}
+        sx={{
+          display: 'flex',
+          mx: 'auto',
+          flexDirection: 'column',
+          alignItems: 'center',
+          gap: 1,
+          p: 3,
+          width: 400,
+        }}
+        onSubmit={handleSubmit}
       >
-        <TextField
-          type="email"
-          label="Email"
-          fullWidth
-        />
-        <TextField
-          type="password"
-          label="Password"
-          fullWidth
-        />
-      </Box>
+        <SecurityIcon color="primary" sx={{ fontSize: 45 }} />
+        <Typography component="h1" variant="h4">
+          Login
+        </Typography>
 
-      <Button variant="contained" size="large">Login</Button>
-
-    </Paper>
-  </Container>
-);
+        <Box sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          gap: 2,
+          width: 1 / 1,
+          my: 2,
+        }}
+        >
+          <TextField
+            type="email"
+            label="Email"
+            fullWidth
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <TextField
+            type="password"
+            label="Password"
+            fullWidth
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Box>
+        <Button variant="contained" size="large" type="submit">Login</Button>
+      </Paper>
+    </Container>
+  );
+};
 
 export default LoginPage;
