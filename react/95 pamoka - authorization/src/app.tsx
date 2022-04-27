b7b58022b672dddefc60c1c63ac84c9e8a8e2d9e
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-04-27 10:20:49 +0300

    task-done(95): problem solved

--- react/95 pamoka - authorization/src/app.tsx
+++ react/95 pamoka - authorization/src/app.tsx
@@ -10,6 +10,7 @@ import LoginPage from './pages/login-page';
 import VisitorLayout from './layouts/visitor-layout';
 import { AuthProvider } from './features/auth/auth-context';
 import RequireAuth from './routing/require-auth';
+import RequireVisitor from './routing/require-visitor';
 
 const App: React.FC = () => (
   <BrowserRouter>
@@ -17,7 +18,14 @@ const App: React.FC = () => (
       <Routes>
         <Route path="/" element={<VisitorLayout />}>
           <Route index element={<HomePage />} />
-          <Route path="auth/login" element={<LoginPage />} />
+          <Route
+            path="auth/login"
+            element={(
+              <RequireVisitor>
+                <LoginPage />
+              </RequireVisitor>
+            )}
+          />
           <Route
             path="profile"
             element={(
