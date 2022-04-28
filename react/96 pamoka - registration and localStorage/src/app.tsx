4025dede3ef5dde0510f0779e3b77929a8e3b68a
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-04-28 08:14:44 +0300

    init(96): localStorage and registration

---
+++ react/96 pamoka - registration and localStorage/src/app.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from 'react-router-dom';
+import HomePage from './pages/home-page';
+import ProfilePage from './pages/profile-page';
+import LoginPage from './pages/login-page';
+import VisitorLayout from './layouts/visitor-layout';
+import { AuthProvider } from './features/auth/auth-context';
+import RequireAuth from './routing/require-auth';
+import RequireVisitor from './routing/require-visitor';
+
+const App: React.FC = () => (
+  <BrowserRouter>
+    <AuthProvider>
+      <Routes>
+        <Route path="/" element={<VisitorLayout />}>
+          <Route index element={<HomePage />} />
+          <Route
+            path="auth/login"
+            element={(
+              <RequireVisitor>
+                <LoginPage />
+              </RequireVisitor>
+            )}
+          />
+          <Route
+            path="profile"
+            element={(
+              <RequireAuth>
+                <ProfilePage />
+              </RequireAuth>
+            )}
+          />
+        </Route>
+      </Routes>
+    </AuthProvider>
+  </BrowserRouter>
+);
+
+export default App;
+
+// Sukurkite apsaugą RequireVisitor LoginPage route'ui
+//    Jeigu prisijungęs vartotojas bando patekti į Visitor rolės puslapį,
+//    jis turi būti nukreipiamas į HomePage
