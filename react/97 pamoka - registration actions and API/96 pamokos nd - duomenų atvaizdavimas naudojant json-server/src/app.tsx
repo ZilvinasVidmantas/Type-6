08a465b2bd9e3a549853f65c35295ae87e567451
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-04-29 08:45:01 +0300

    feat(96): homework example

---
+++ react/97 pamoka - registration actions and API/96 pamokos nd - duomenų atvaizdavimas naudojant json-server/src/app.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from 'react-router-dom';
+
+import HomePage from './pages/home-page';
+
+const App: React.FC = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+    </Routes>
+  </BrowserRouter>
+);
+
+export default App;
