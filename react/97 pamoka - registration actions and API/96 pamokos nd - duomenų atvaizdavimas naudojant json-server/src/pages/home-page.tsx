bf51aa0ef36e09cd8adbb1a0ea6f8932032aec79
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-04-29 09:20:41 +0300

    feat(96): data view

--- react/97 pamoka - registration actions and API/96 pamokos nd - duomenų atvaizdavimas naudojant json-server/src/pages/home-page.tsx
+++ react/97 pamoka - registration actions and API/96 pamokos nd - duomenų atvaizdavimas naudojant json-server/src/pages/home-page.tsx
@@ -1,16 +1,79 @@
+import React, { useState, useEffect } from 'react';
 import {
   Container,
+  Box,
   Paper,
   Typography,
+  Grid,
+  Button,
+  styled,
 } from '@mui/material';
-import React from 'react';
 
-const HomePage: React.FC = () => (
-  <Container sx={{ my: 5 }}>
-    <Paper sx={{ p: 5 }}>
-      <Typography component="h1" variant="h3">React CodeAcademy page</Typography>
-    </Paper>
-  </Container>
-);
+type Programmer = {
+  name: string,
+  surname: string,
+  languages: string[],
+  technologies: string[],
+  img: string,
+  yearsOfExperience: number,
+};
+
+const Img = styled('img')({
+  width: '100%',
+  height: 270,
+  objectFit: 'cover',
+  objectPosition: 'top',
+});
+
+const HomePage: React.FC = () => {
+  const [programmers, setProgrammers] = useState<Programmer[]>([]);
+
+  useEffect(() => {
+    fetch('http://localhost:8000/programmers')
+      .then((response) => response.json())
+      .then((fetchedProgrammers) => {
+        setProgrammers(fetchedProgrammers as Programmer[]);
+      })
+      .catch(console.error);
+  }, []);
+
+  return (
+    <Container sx={{ my: 5 }}>
+      <Typography component="h1" variant="h2" sx={{ textAlign: 'center', mb: 3 }}>
+        {programmers.length === 0 ? 'You have no geeks' : 'Your geeks'}
+      </Typography>
+      <Grid container spacing={3}>
+        {programmers.map(({
+          name, surname, languages, technologies, img, yearsOfExperience,
+        }) => (
+          <Grid item xs={4}>
+            <Paper sx={{ p: 3 }} elevation={3}>
+              <Img src={img} alt="" />
+              <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
+                <Typography variant="h5" sx={{ textAlign: 'center' }}>{`${name} ${surname}`}</Typography>
+                <Box>
+                  <Typography variant="h6">Languages</Typography>
+                  <Typography sx={{ pl: 3 }}>{languages.join(', ')}</Typography>
+                </Box>
+                <Box>
+                  <Typography variant="h6">Technologies</Typography>
+                  <Typography sx={{ pl: 3 }}>{technologies.join(', ')}</Typography>
+                </Box>
+                <Box>
+                  <Typography variant="h6">Experience</Typography>
+                  <Typography sx={{ pl: 3 }}>{`${yearsOfExperience} years`}</Typography>
+                </Box>
+                <Box sx={{ display: 'flex', justifyContent: 'center', gap: 1 }}>
+                  <Button variant="contained" color="primary">I am crazy</Button>
+                  <Button variant="contained" color="info">Call me maybe</Button>
+                </Box>
+              </Box>
+            </Paper>
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
+  );
+};
 
 export default HomePage;
