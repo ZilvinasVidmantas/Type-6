d311d92e8d842f654b84a197ec5d59a79e6aa6ec
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-05-02 11:37:04 +0300

    feat(98): profile page updated using context

--- react/98 pamoka - json server mock authorization/src/pages/profile-page/index.tsx
+++ react/98 pamoka - json server mock authorization/src/pages/profile-page/index.tsx
@@ -1,16 +1,25 @@
-import React from 'react';
-import { Typography, Container } from '@mui/material';
+import React, { useContext } from 'react';
+import { Typography, Container, Box } from '@mui/material';
+import AuthContext from '../../features/auth/auth-context';
+import Img from '../../components/img';
 
-const ProfilePage: React.FC = () => (
-  <Container>
-    <Typography
-      component="h1"
-      variant="h1"
-      sx={{ textAlign: 'center' }}
-    >
-      This is profile page
-    </Typography>
-  </Container>
-);
+const ProfilePage: React.FC = () => {
+  const { user } = useContext(AuthContext);
+
+  return (
+    <Container>
+      <Typography
+        component="h1"
+        variant="h3"
+        sx={{ textAlign: 'center' }}
+      >
+        {`Hello, ${user?.name} ${user?.surname}`}
+      </Typography>
+      <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+        <Img src={user?.img} />
+      </Box>
+    </Container>
+  );
+};
 
 export default ProfilePage;
