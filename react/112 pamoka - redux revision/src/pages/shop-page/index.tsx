f146a10a91c16df30f22ed021747fc0370f9624b
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-05-20 11:14:32 +0300

    feat(112): placed items into database

--- react/112 pamoka - redux revision/src/pages/shop-page/index.tsx
+++ react/112 pamoka - redux revision/src/pages/shop-page/index.tsx
@@ -3,31 +3,30 @@ import {
   Typography,
   Container,
   Grid,
-  Box,
 } from '@mui/material';
 
 import { useRootSelector } from '../../store';
 import ShopPageCard from './shop-page-card';
-import { selectCartItems, selectShopItems } from '../../store/selectors';
+import { selectShopItems } from '../../store/selectors';
 
 const ShopPage: React.FC = () => {
   const shopItems = useRootSelector(selectShopItems);
-  const cartItems = useRootSelector(selectCartItems);
 
   return (
     <Container>
-      <Typography component="h1" variant="h2">Shop</Typography>
-      <Grid container spacing={4}>
-        {shopItems.map((item) => (
-          <Grid item key={item.id} xs={4}>
-            <ShopPageCard {...item} />
+      <Typography component="h1" variant="h2" sx={{ my: 3 }}>Shop</Typography>
+      {shopItems.length > 0
+        ? (
+
+          <Grid container spacing={4}>
+            {shopItems.map((item) => (
+              <Grid item key={item.id} xs={4}>
+                <ShopPageCard {...item} />
+              </Grid>
+            ))}
           </Grid>
-        ))}
-      </Grid>
-      <Typography component="h2" variant="h2">Cart</Typography>
-      <Box component="pre">
-        {JSON.stringify(cartItems, null, 2)}
-      </Box>
+        )
+        : <Typography component="h2" variant="h3" sx={{ my: 3 }}>No items, sorry.</Typography>}
     </Container>
   );
 };
--- react/112 pamoka - redux revision/src/store/features/shop/shop-reducer.ts
+++ react/112 pamoka - redux revision/src/store/features/shop/shop-reducer.ts
@@ -3,42 +3,7 @@ import { Reducer } from 'redux';
 import { ShopState, ShopAction } from './types';
 
 const initialState: ShopState = {
-  items: [
-    {
-      id: '1',
-      images: [
-        'https://www.ubuy.vn/productimg/?image=aHR0cHM6Ly9tLm1lZGlhLWFtYXpvbi5jb20vaW1hZ2VzL0kvNTFhcUNwa2xJSUwuX0FDX1NMMTAwMF8uanBn.jpg',
-      ],
-      price: 199.99,
-      categories: ['artilery', 'hobies'],
-      amount: 0,
-      additionalProps: {
-        arrowCount: '27',
-        distance: '200m',
-      },
-    },
-    {
-      id: '2',
-      images: [
-        'https://www.ubuy.com/productimg/?image=aHR0cHM6Ly9tLm1lZGlhLWFtYXpvbi5jb20vaW1hZ2VzL0kvNjFINEpETHJHREwuX0FDX1NMMTAwMF8uanBn.jpg',
-      ],
-      price: 39.99,
-      categories: ['artilery', 'hobies'],
-      amount: 24,
-    },
-    {
-      id: '3',
-      images: [
-        'https://i0.wp.com/sabersmith.com/wp-content/uploads/2020/07/Hand-Half-Gold-Black-on-Green-with-Wire.jpg?fit=800%2C1200&ssl=1',
-      ],
-      price: 479.99,
-      categories: ['close-combat', 'hobies'],
-      amount: 4,
-      additionalProps: {
-        length: '1.2m',
-      },
-    },
-  ],
+  items: [],
 };
 
 const shopReducer: Reducer<ShopState, ShopAction> = (state = initialState, action) => {
