57d350ce196e90b9891b3abed66f4e6318b24aa8
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-05-25 08:29:39 +0300

    feat(115): Link to shop page

--- react/115 pamoka - final example - cart page redux actions/src/pages/cart-page/index.tsx
+++ react/115 pamoka - final example - cart page redux actions/src/pages/cart-page/index.tsx
@@ -1,19 +1,27 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import {
   Container,
   Typography,
+  Button,
 } from '@mui/material';
 import CartPageItemGrid from './cart-page-item-grid';
 import { useRootSelector } from '../../store/hooks';
 import { selectCartItemsCount } from '../../store/features/cart/cart-selectors';
 
 const CartPage: React.FC = () => {
+  const navigate = useNavigate();
   const itemsCount = useRootSelector(selectCartItemsCount);
 
   return (
     <Container>
       <Typography component="h1" variant="h2" sx={{ my: 3, mb: 5 }}>Cart</Typography>
-      {itemsCount > 0 ? <CartPageItemGrid /> : <Typography>Nėra aitemų</Typography>}
+      {itemsCount > 0 ? <CartPageItemGrid /> : (
+        <>
+          <Typography variant="h3" sx={{ mb: 3 }}>Krepšelis tuščias 😒</Typography>
+          <Button variant="contained" onClick={() => navigate('/shop')}>← Rinktis prekes</Button>
+        </>
+      )}
 
     </Container>
   );
