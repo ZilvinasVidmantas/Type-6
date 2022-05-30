256c642559a4f2fe3d2d4495cf0c0fccef168c65
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-05-30 11:40:39 +0300

    task(118): Express request handlers

--- express/118 pamoka - express server configuration/src/server.ts
+++ express/118 pamoka - express server configuration/src/server.ts
@@ -9,3 +9,18 @@ server.use(express.static('public'));
 server.use('/api/products', productsRouter);
 
 server.listen(1337, () => console.log(`Server is running on: http://localhost:1337`));
+
+/*
+  Sukurkite savo sugalvotos kolekcijos parsiuntimą ir įrašo trynima:
+    * sukurkite tam atskirą router, nurodant maršrutus
+    * sukurkite tam atskirą controller, aprašant logiką
+    * Susikurkite Postman programoje užklausas ir jas išsaugokite
+    
+  Užduotis atlikte TDD (Test Driven Development principu):
+    * Vykdykite planuojamas užklausas tam tikrai adresas, gaudami neigiamą Express žinutę
+    * Parašykite maršrutus, kur aptarnautų užklausas
+    * Aprašykite užklausų aptarnavimą, teisingų duomenų atveju
+    * Aprašykite užklausų aptarnavimą, neteisingų duomenų atveju
+    * Struktūrizuokite kodą
+    * Vėl patikrinkite, ar teisingai veikia kodas
+*/
