036717ce3c58e805f3cde6bcdb54334df8421ae8
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-06-01 11:11:58 +0300

    feat(120): validated categories for update product request handler

--- express/120 pamoka - DB relations/src/controllers/products-controller/index.ts
+++ express/120 pamoka - DB relations/src/controllers/products-controller/index.ts
@@ -4,6 +4,24 @@ import ProductModel from "../../models/product-model";
 import CategoryModel from "../../models/category-model";
 import { formatProductValidationError } from './products-error-formatters';
 
+const validateCategoriesIds = async (categoriesIds: string[]) => {
+  if (categoriesIds.length > 0) {
+
+    const uniqCategoryIds = [...new Set(categoriesIds)];
+    const foundCategories = await CategoryModel.find({
+      // Ar yra tokių kategorių, kurių id yra viena iš <categories> masyve esančių reikšmių?
+      _id: { $in: uniqCategoryIds },
+    });
+
+    if (uniqCategoryIds.length !== foundCategories.length) {
+      throw new Error("Dalis kategorijų neegzistuoja");
+    }
+
+    return uniqCategoryIds;
+  }
+  return [];
+}
+
 export const getProducts: RequestHandler = async (req, res) => {
   const products = await ProductModel.find();
 
@@ -40,17 +58,8 @@ export const updateProduct: RequestHandler = async (req, res) => {
   const productProps = req.body;
 
   try {
-    const { categories } = productProps;
-    if (categories.length > 0) {
-
-      const foundCategories = await CategoryModel.find({
-        // Ar yra tokių kategorių, kurių id yra viena iš <categories> masyve esančių reikšmių?
-        _id: { $in: categories },
-      });
-      if (categories.length !== foundCategories.length) {
-        throw new Error("Dalis kategorijų neegzistuoja");
-      }
-    }
+    const uniqCategoriesIds = await validateCategoriesIds(productProps.categories);
+    productProps.categories = uniqCategoriesIds;
     const updatedProduct = await ProductModel.findByIdAndUpdate(id, productProps, { new: true });
     res.status(200).json(updatedProduct);
   } catch (error) {
